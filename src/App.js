05be4adbe2e29c159fb55c0d49463fb3238adade
norm02 <richa0920@gmail.com>
2023-01-31 07:43:36 +0900

    station3

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ export const App = () => {
   return (
     <header>
       <h1>Dogアプリ</h1>
+      <description>犬の画像を表示するアプリです</description>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
     </header>
   )
 }
