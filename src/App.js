2dfa27da6dc16368fffe351850a0bad810ea697f
norm02 <richa0920@gmail.com>
2023-01-31 08:45:30 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 /**
@@ -8,11 +8,20 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogurl, setDogurl] = useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
   return (
     <header>
       <h1>Dogアプリ</h1>
       <description>犬の画像を表示するアプリです</description>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
+      <input
+        type="text"
+        placeholder="犬の画像"
+        onChange={e => setDogurl(e.target.value)}
+      />
+      (
+      <img src={dogurl} alt="Displayed" />)
     </header>
   )
 }
