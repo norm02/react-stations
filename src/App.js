84e9f38261de3d35a5a811bc37b6aacf750d6c0b
norm02 <richa0920@gmail.com>
2023-01-31 10:19:44 +0900

    station5

--- src/App.js
+++ src/App.js
@@ -11,17 +11,22 @@ export const App = () => {
   const [dogurl, setDogurl] = useState(
     'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
   )
+  console.log('click')
+
+  const buttonclick = () => {
+    const newdogurl =
+      'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg'
+    setDogurl(newdogurl)
+  }
+
   return (
     <header>
       <h1>Dogアプリ</h1>
       <description>犬の画像を表示するアプリです</description>
-      <input
-        type="text"
-        placeholder="犬の画像"
-        onChange={e => setDogurl(e.target.value)}
-      />
-      (
-      <img src={dogurl} alt="Displayed" />)
+      <img src={dogurl} alt="Displayed" />
+      <button type="button" onClick={e => setDogurl(buttonclick)}>
+        更新
+      </button>
     </header>
   )
 }
