43c0abf846617ff68f8bc98ec46039c6e368b0a0
norm02 <richa0920@gmail.com>
2023-02-01 09:14:42 +0900

    station9

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { Header } from './Header.js'
 import './App.css'
 import { Description } from './Description.js'
+import { DogListContainer } from './DogListContainer.js'
 
 /**
  *
@@ -14,6 +15,7 @@ export const App = () => {
     <div>
       <Header />
       <Description />
+      <DogListContainer />
     </div>
   )
 }
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,24 @@
 // DO NOT DELETE
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+export const DogListContainer = () => {
+  const [breeds, setBreeds] = useState([])
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(res => res.json())
+      .then(result => {
+        const list = result.message
+        setBreeds(Object.keys(list))
+      })
+  }, [])
+  return (
+    <select name="breed">
+      {breeds.map(breed => (
+        <option key={breed} value={breed}>
+          {breed}
+        </option>
+      ))}
+    </select>
+  )
+}
