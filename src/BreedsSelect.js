7816c803fcc54ad82e8ac3bd7b0e344da0bd274f
norm02 <richa0920@gmail.com>
2023-02-05 12:47:16 +0900

    station10

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,15 @@
 // DO NOT DELETE
+import * as React from 'react'
+import './App.css'
+export const BreedsSelect = props => {
+  const breedlist = props.breeds.map(breed => (
+    <option key={breed} value={breed}>
+      {breed}
+    </option>
+  ))
+  return (
+    <select value={props.select} onChange={props.change}>
+      {breedlist}
+    </select>
+  )
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,9 +1,11 @@
 // DO NOT DELETE
 import React, { useEffect, useState } from 'react'
+import { BreedsSelect } from './BreedsSelect'
 import './App.css'
 
 export const DogListContainer = () => {
   const [breeds, setBreeds] = useState([])
+  const [selectedBreed, setSelectedBreed] = useState('')
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then(res => res.json())
@@ -12,13 +14,12 @@ export const DogListContainer = () => {
         setBreeds(Object.keys(list))
       })
   }, [])
+  const onchange_handle = e => setSelectedBreed(e.target.value)
   return (
-    <select name="breed">
-      {breeds.map(breed => (
-        <option key={breed} value={breed}>
-          {breed}
-        </option>
-      ))}
-    </select>
+    <BreedsSelect
+      breeds={breeds}
+      select={selectedBreed}
+      change={onchange_handle}
+    ></BreedsSelect>
   )
 }
