3831dc2020363a7a4305cdbc9df194b930debec0
bono <62996844+norm02@users.noreply.github.com>
2023-02-05 13:59:27 +0900

    station12

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -5,7 +5,9 @@ import './App.css'
 
 export const DogListContainer = () => {
   const [breeds, setBreeds] = useState([])
-  const [selectedBreed, setSelectedBreed] = useState('')
+  const [selectedBreed, setSelectedBreed] = useState('borzoi')
+  const [dogList, setdogList] = useState([])
+  const onchange_handle = e => setSelectedBreed(e.target.value)
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then(res => res.json())
@@ -14,12 +16,33 @@ export const DogListContainer = () => {
         setBreeds(Object.keys(list))
       })
   }, [])
-  const onchange_handle = e => setSelectedBreed(e.target.value)
+
+  function getAPI(type) {
+    fetch('https://dog.ceo/api/breed/' + type + '/images/random/12')
+      .then(res => res.json())
+      .then(result => {
+        setdogList(result.message)
+      })
+  }
+  const breedImages = dogList.map(image => {
+    return (
+      <div className="dogImage" key={Image}>
+        <img src={Image} key={Image} />
+      </div>
+    )
+  })
+
   return (
-    <BreedsSelect
-      breeds={breeds}
-      select={selectedBreed}
-      change={onchange_handle}
-    ></BreedsSelect>
+    <div>
+      <BreedsSelect
+        breeds={breeds}
+        select={selectedBreed}
+        change={onchange_handle}
+      ></BreedsSelect>
+      <button onClick={() => getAPI(selectedBreed)}>表示</button>
+      <br />
+      {breedImages}
+    </div>
   )
 }
+
